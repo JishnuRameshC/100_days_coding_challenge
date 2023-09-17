7c0994c216cee2c732accf29f6dd51b780ec993e
jishnu ramesh c <jishnurameshc@gmail.com>
2023-09-17 08:27:02 +0530

    url building and templating with jinja

--- day_57/blog-templating-start/main.py
+++ day_57/blog-templating-start/main.py
@@ -5,20 +5,18 @@ import requests
 app = Flask(__name__)
 
 api = "https://api.npoint.io/c790b4d5cab58020d391"
-
+response = requests.get(api)
+data = response.json()
 
 @app.route('/')
 def index():
-    response = requests.get(api)
-    data = response.json()
-    return render_template('index.html',datas= data)
+    return render_template('index.html',all_data= data)
 
 
 @app.route('/post/<int:id>')
 def get_post(id):
-    response = requests.get(api)
-    data = response.json()[id-1]
-    return render_template('post.html',data=data)
+    global data
+    return render_template('post.html',data=data[id-1])
 
 
 if __name__ == "__main__":
