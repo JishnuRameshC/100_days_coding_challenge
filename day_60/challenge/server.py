2179ad25bf2fa3b4c0910bd72a19512d525c6ed1
jishnu ramesh c <jishnurameshc@gmail.com>
2023-09-20 15:22:54 +0530

    using smptlib send an email to ourselves with the data submitted by the user

---
+++ day_60/challenge/server.py
@@ -0,0 +1,34 @@
+from flask import Flask,render_template,request
+import smtplib
+from secret import my_email,my_password
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+connecting = smtplib.SMTP("smtp.gmail.com", 587)
+connecting.starttls()
+connecting.login(user=my_email,password=my_password)
+
+
+@app.route('/login',methods=['post'])
+def home():
+    name = request.form["name"]
+    phoneno = request.form["phoneno"]
+    email = request.form["email"]
+    message = request.form["message"]
+    # sending mail
+    connecting = smtplib.SMTP("smtp.gmail.com", 587)
+    connecting.starttls()
+    connecting.login(user=my_email,password=my_password)
+    connecting.sendmail(from_addr=my_email,to_addrs=email,msg=f"from:{name}\n {phoneno}\n\n{message}")
+    connecting.close()
+    print(f"succes name ={name} \n {phoneno} \n {email} \n {message} ")
+    return f"<h1> Succesfuly send your message"
+
+
+if __name__ == "__main__":
+    app.run()
